1c82e85d6ae9dc0675d66bdde7fbf72ec789a487
Anadyr <anadyrskie@anadyr.us>
2020-10-21 03:14:01 -0800

    add documentation

--- auth.php
+++ auth.php
@@ -1,11 +1,13 @@
 <?php
+
+    // login object
     class login{
         public $username = NULL;
         public $password = NULL;
         public $token = NULL;
     }
 
-
+    // auth object
 class auth{
     public $userid = NULL;
     public $key = NULL;
@@ -13,21 +15,15 @@ class auth{
 }
 $KEY_LENGTH = 16;
 
+    // Generates and returns an API key from random bytes
 function generateKey(){
-    /* generateKey()
-     * Input:
-     *     None
-     * Output:
-     *     Generates an API key with random bytes and a timeout timestamp
-     * Notes:
-     *     This function is used in the checkLogin function
-     */
     global $KEY_LENGTH;
         return strval(bin2hex(random_bytes($KEY_LENGTH)));
 
 
 }
 
+    // Generates and returns token
 function generateToken(){
     global $KEY_LENGTH;
 
@@ -35,6 +31,7 @@ function generateToken(){
 
 }
 
+    // sets and returns the auth object
 function setKey($authObj){
     $authObj->userid = 0;
     $authObj->key = generateKey();
@@ -42,18 +39,23 @@ function setKey($authObj){
     return $authObj;
 }
 
-
-
-
-function checkLogin($login_obj){
-    /* checkLogin($login_obj)
+/* checkLogin($login_obj)
      * Input: Login Object
+     *
+     *Logic:
+     *     Case 1: Username and Token
+     *          sets auth key and returns same token
+     *     Case 2: Username and password
+     *          sets and returns token and auth key
+     *
      * Output:
      *     Case 1: Login success
      *         returns $auth_obj with userid
      *     Case 2: Login failure
-     *         returns NULL
-     */
+     *         returns 1
+ */
+function checkLogin($login_obj){
+
 
     if(strcmp(strval($login_obj->username), 'admin')== 0 && strcmp(strval($login_obj->token), 'bz0CqegTN5KjdNubxccBoTTauMnQA5ijUHWaMSfwdWUeeqBubetfeZba6UCmGSl6') == 0){
         $authObj = new auth;
@@ -72,43 +74,54 @@ function checkLogin($login_obj){
 }
 
 
-
+/* Handles "submit=" requests in the POST body
+    * Instantiates and populates a login object and passes it to checkLogin
+    * Instantiates an auth object and populates it with the login response
+    * If login was unsuccessful, returns "Forbidden"
+ */
     function requestHandler($request_type){
     if ($request_type == 'login'){
-        //echo 'login triggered <br>';
-
         # Get login information
         $login_obj = new login;
         $login_obj->username = htmlspecialchars($_POST['username']);
         $login_obj->password = htmlspecialchars($_POST['password']);
         $login_obj->token = htmlspecialchars($_POST['token']);
         $auth_obj = checkLogin($login_obj);
-        /*if ($auth_obj == 1){
-            return http_response_code(403);
-        }*/
-        #else {
-            return $auth_obj;
-        #}
-    }
+        return $auth_obj;
 
+    }
+    //returns when the if statements are skipped or all evaluate false. Mainly for debugging.
     return "something went wrong";
 }
 
+/* Main function
+ *      First checks the HTTP request type to see if it conforms,
+ *      then checks the request body to verify conformity.
+ *      It passes the submit type to requestHandler()
+ *      and returns the JSON-encoded result in the response body.
+ *
+ *      If the:
+ *              request type is wrong, it returns "Incorrect Method"
+ *              request body is formatted incorrectly, it responds with "bad request"
+ *              supplied credentials are wrong, it replies with "unauthorized"
+ */
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if(isset($_POST['submit'])) {
         $request_type = $_POST['submit'];
         if ($request_type !== NULL) {
             $response = requestHandler($request_type);
-            echo json_encode($response);
+
+            if(is_object($response)){ echo json_encode($response) . "<br />" . http_response_code(201); }
+            else {echo http_response_code(401);}
         } else {
-            echo http_response_code(400);
+            echo http_response_code(401);
         }
     }
     else {
-        echo http_response_code(406);
+        echo http_response_code(400);
     }
 }
 else {
-    echo http_response_code(404);
+    echo http_response_code(405);
 }
