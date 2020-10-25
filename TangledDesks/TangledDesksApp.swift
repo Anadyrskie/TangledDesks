39ae735c6ce636d526f54ea7e16e2279f3495f32
Anadyr <anadyrskie@anadyr.us>
2020-10-24 21:01:27 -0800

    Initial Commit

---
+++ TangledDesks/TangledDesksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangledDesksApp.swift
+//  TangledDesks
+//
+//  Created by Anadyr on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct TangledDesksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
